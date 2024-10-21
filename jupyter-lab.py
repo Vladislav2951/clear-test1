21a6146d6f2556a7ff63454e73da6c53329f68a0
Vladislav <95657925+Vladislav2951@users.noreply.github.com>
2024-10-21 13:44:36 +0300

    Update jupyter-lab.py

--- jupyter-lab.py
+++ jupyter-lab.py
@@ -4,8 +4,5 @@ import os
 # Инициализация задачи ClearML
 task = Task.init(project_name="My Project", task_name="Run JupyterLab in Docker")
 
-# Настройка Docker-контейнера с установленным JupyterLab
-task.set_base_docker("jupyter/base-notebook:latest")
-
 # Запуск JupyterLab
 os.system("jupyterlab --ip=0.0.0.0 --allow-root --no-browser")
