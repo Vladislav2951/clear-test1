90be61ca1594c3135da89684cad3039b36deaee1
Vladislav <95657925+Vladislav2951@users.noreply.github.com>
2024-10-21 13:43:55 +0300

    Update jupyter-lab.py

--- jupyter-lab.py
+++ jupyter-lab.py
@@ -7,8 +7,5 @@ task = Task.init(project_name="My Project", task_name="Run JupyterLab in Docker"
 # Настройка Docker-контейнера с установленным JupyterLab
 task.set_base_docker("jupyter/base-notebook:latest")
 
-# Запуск задачи на удаленном агенте (при использовании ClearML агент будет запускать задачу внутри Docker)
-task.execute_remotely(queue_name="default", exit_process=False)
-
 # Запуск JupyterLab
-os.system("jupyter lab --ip=0.0.0.0 --allow-root --no-browser")
+os.system("jupyterlab --ip=0.0.0.0 --allow-root --no-browser")
