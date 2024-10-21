3766cf31d6126d83922842816f9be04852cb21ce
Vladislav <95657925+Vladislav2951@users.noreply.github.com>
2024-10-21 14:05:38 +0300

    Update jupyter-lab.py

--- jupyter-lab.py
+++ jupyter-lab.py
@@ -5,4 +5,4 @@ import os
 task = Task.init(project_name="My Project", task_name="Run JupyterLab in Docker")
 
 # Запуск JupyterLab
-os.system("jupyter-lab --ip=0.0.0.0 --allow-root --no-browser")
+os.system("jupyter lab --ip=0.0.0.0 --allow-root --no-browser")
